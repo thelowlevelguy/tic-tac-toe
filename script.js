3f4e29a9aef7781b0c0ea1e295b221b13f31d340
thelowlevelguy <gkalla013@gmail.com>
2026-08-14 21:54:03 +0000

    feat(GameBoard): add form dialog for user name , display users name's & winner

--- script.js
+++ script.js
@@ -29,26 +29,93 @@ const GameBoard  = (() => {
 const Players = (() => {
 	const player1 = {
 		name : "X",
-		username: "",
+		mark : "X"
 	}
 	const player2 = {
 		name : 'O',
-		username: "",
+		mark : "O",
 	}
-	return {player1, player2};
+
+	const setPlayerName = (player, name) => {
+		player.name = name;
+	}
+	return {player1, player2, setPlayerName};
 })()
 
 const BoardDom = (() => {
+	const container = document.getElementById("container")
 	const boardContainer = document.getElementById("boardContainer");
-	const renderBoard = (board) => {
+	const renderFormDialog = () => {
+		
+		const usernameDialog = document.createElement("dialog")
+		usernameDialog.id = "username-dialog";
 
+		const form = document.createElement("form")
+		form.id = "user-form"
+		const playerXBlock = document.createElement("div")
+		const playerXLabel = document.createElement("label")
+		const playerXInput = document.createElement("input")
+
+		playerXLabel.textContent = "Player X";
+		playerXInput.style.placeholder = "username"
+
+		playerXLabel.for = "Player-X"
+		playerXInput.name = "Player-X"
+
+		playerXBlock.appendChild(playerXLabel)
+		playerXBlock.appendChild(playerXInput)
+		form.appendChild(playerXBlock)
+
+		const playerOBlock = document.createElement("div")
+		const playerOLabel = document.createElement("label")
+		const playerOInput = document.createElement("input")
+
+		playerOLabel.textContent = "Player O";
+		playerOInput.style.placeholder = "username"
+
+		playerOLabel.for = "Player-O"
+		playerOInput.name = "Player-O"
+
+		playerOBlock.appendChild(playerOLabel)
+		playerOBlock.appendChild(playerOInput)
+		form.appendChild(playerOBlock)
+
+		const button = document.createElement("button")
+		button.type = "submit"
+		button.textContent = "Go"
+		form.appendChild(button)
+
+		usernameDialog.appendChild(form)
+		container.appendChild(usernameDialog)
+		usernameDialog.showModal()
+	}
+
+	const setUsername = () => {
+		const dialog = document.getElementById("username-dialog")
+		const form =  dialog.querySelector("#user-form")
+		const formData = new FormData(form);
+		let playerX = formData.get("Player-X") || "X";
+		let playerO = formData.get("Player-O") || "O";	
+		document.getElementById("player1").textContent = playerX;
+		document.getElementById('player2').textContent = playerO;
+		dialog.close()
+		return [playerX, playerO]
+	}
+
+	const closeDialog = () => {
+		const dialog = document.getElementById("username-dialog")
+		dialog.close()
+		container.removeChild(dialog)
+	}
+
+	const renderBoard = (board) => {
 		boardContainer.textContent = "";
 		const boardDiv = document.createElement("div");
 		boardDiv.setAttribute("id", "board");
 
 		board.forEach((row, rowIndex) => {
 			const rowDiv = document.createElement("div");
-			rowDiv.setAttribute("class", "row")
+			rowDiv.setAttribute("class", "row");
 			row.forEach((column, colIndex) => {
 				const columnDiv = document.createElement("div");
 				columnDiv.setAttribute("class", "column")
@@ -72,50 +139,34 @@ const BoardDom = (() => {
 		winnerText.setAttribute("id", "winner-text")
 		winnerText.textContent = `${winner} Win!`
 		winnerBoard.appendChild(winnerText)
-		winnerBoard.style.border = "none"
-		winnerBoard.style.width = "300px"
-		winnerBoard.style.heigth = "100px"
-		winnerBoard.style.display = "flex";
-		winnerBoard.style.justifyContent = "center"
-		winnerBoard.style.alignItems = "center"
 
-
-		const style = document.createElement('style');
- 		style.textContent = `
-
-    		#winner-board::backdrop {
-     		background: rgba(0, 0, 0, 0.6);
-      		backdrop-filter: blur(3px);
-
-    	}`;
-
-    	document.head.appendChild(style)
     	boardContainer.appendChild(winnerBoard)
     	winnerBoard.showModal();
 
 		setTimeout(() => {
 			boardContainer.removeChild(winnerBoard)
-			document.head.removeChild(style)
 		}, 2000)
 	}
 
-	const resetBoard = () => {
-
+	const resetBoard = (board) => {
+		renderBoard(board);
 	}
 
-	return {renderBoard, markCell, displayWinner, renderBoard}
+	return {renderFormDialog, setUsername, closeDialog, markCell, displayWinner, renderBoard}
 })()
 
 const gameController = (() => {
-	const playerX = Players.player1;
-	const playerO = Players.player2;
+	let playerX = Players.player1;
+	let playerO = Players.player2;
+
 	//goole says this one plays first usually
-	let turn = playerX.name;
+	let turn = playerX.mark;
 
 	GameBoard.initBoard();
 	let board = GameBoard.getBoard();
 	//get row's length from the first one
 	const lenRow = board[0].length;
+
 
 	//check winner
 	const alignWin = (allLines) => {
@@ -166,8 +217,20 @@ const gameController = (() => {
 		const allLines = [...horizontal, ...vertical, ...diagonal]
 		return alignWin(allLines)
 	} 
+	BoardDom.renderFormDialog();
+	BoardDom.renderBoard(board);
 
 	const insertionControl = () => {
+		const dialog = document.getElementById("username-dialog")
+		dialog.addEventListener("submit", (event) => {
+			event.preventDefault();
+			event.preventDefault();
+			players = BoardDom.setUsername()
+			playerX.name = players[0]
+			playerO.name = players[1]
+			BoardDom.closeDialog()
+		})
+		
 		//check who's player get the turn
 		//const user clicked cell by row and column
 		let gameOver = false;
@@ -191,14 +254,15 @@ const gameController = (() => {
 					gameOver = true
 					controller.abort();
 					//display winner for 2 secs
-					BoardDom.displayWinner(turn);
+					let winner = turn === playerO.mark ? playerO.name : playerX.name
+					BoardDom.displayWinner(winner);
 					return
 				}
-				turn = turn === playerO.name ? playerX.name : playerO.name;
+				turn = turn === playerO.mark ? playerX.mark : playerO.mark;
 			}
 		}, {signal : controller.signal})		
 	}
-	BoardDom.renderBoard(board);
+
 	insertionControl()
 
 })()
